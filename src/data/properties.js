351b65f55fca6347a88641c4dceca43deb961ada
RyoLinEth <ryolindev@gmail.com>
2023-06-02 08:58:11 +0800

    InitProject

---
+++ src/data/properties.js
@@ -0,0 +1,29 @@
+module.exports = [
+  {
+    id: 1,
+    img: "/assets/images/property/fp1.jpg",
+    price: "120",
+    type: "Hololive",
+    title: "Relax Time 白上フブキ",
+    featured: '1',
+    garages: 'F',
+  },
+  {
+    id: 2,
+    img: "/assets/images/property/fp1.jpg",
+    price: "100",
+    type: "Hololive",
+    title: "Relax Time 湊あくあ ",
+    featured: '1',
+    garages: 'F',
+  },
+  {
+    id: 3,
+    img: "/assets/images/property/fp1.jpg",
+    price: "120",
+    type: "One Piece",
+    title: "魯夫",
+    featured: '1',
+    garages: 'M',
+  },
+];
